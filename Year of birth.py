b6c20f421b4ef2d386e0f133cc51aa00e50635ec
aprossie <44318433+aprossie@users.noreply.github.com>
2018-11-19 20:18:07 +0300

    Add files via upload

---
+++ Year of birth.py
@@ -0,0 +1,18 @@
+print("Enter your year of birth")
+Year =int (input ("Year= "))
+days= year*365
+Months = year/12
+print(year,"was born on",day,"days",Month, "Months and",Year,"Year!")
+
+print("user_string")
+user_string ="Minor"
+print (Minor)
+
+my_string = 'Hello'
+print(my_string)
+
+my_string = "Hello"
+print(my_string)
+
+my_string = '''Hello'''
+print(my_string)
